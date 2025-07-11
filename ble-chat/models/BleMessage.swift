e295fb1554e0fcab7a31dcbee1deae15872de476
Jacob Senecal <jacobsenecal@Jacobs-MacBook-Air.local>
2025-07-11 11:25:41 -0600

    test lint v2

--- ble-chat/models/BleMessage.swift
+++ ble-chat/models/BleMessage.swift
@@ -5,7 +5,16 @@
 //  Created by Jacob Senecal on 7/11/25.
 //
 
+import Foundation
+
 struct BleMessage: Equatable, Codable {
-    let id: String
-    let content: String
+  let id: String
+  let content: String
+  let createdAt: Date
+
+  init(id: String, content: String, createdAt: Date) {
+    self.id = id
+    self.content = content
+    self.createdAt = createdAt
+  }
 }
